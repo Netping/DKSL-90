b91cbbd054c997c8fba8369938f0f3435e18a25f
@4d4441 <4d4441@gmail.com>
2021-04-22 17:08:25 +0000

    Env .iso file

--- build.sh
+++ build.sh
@@ -11,17 +11,28 @@ fi
 apt update
 apt install -y rsync cloud-image-utils isolinux xorriso mc netcat git
 
-#mount iso - example ubuntu-20.04.1-live-server-amd64.iso 
-#ToDo download and mount iso 
-#mount /dev/dvd /mnt
+cd /tmp && rm -rf ./iso*
+mkdir ./iso && chmod 777 ./iso
 
+#mount if iso exist and download & mount iso if not exist   
+if [ -f $(basename -- $UBUNTU_ISO) ]
+then
+  mount ./$(basename -- $UBUNTU_ISO) /mnt
+else
+  wget $UBUNTU_ISO
+  mount ./$(basename -- $UBUNTU_ISO) /mnt
+fi
 
-cd /tmp && rm -rf ./iso
-mkdir ./iso && chmod 777 ./iso  
-#rsync -av --progress /mnt/ ./iso/
+rsync -av --progress /mnt/ ./iso/
 
-#clone repo with configs
-git clone git@github.com:Netping/DKSF_90.git ./iso
+#clone repo with configs and sync with iso
+git clone git@github.com:Netping/DKSF_90.git ./isogit
+
+rsync -vr ./isogit/iso/ ./iso/
+rm -rf ./isogit 
+
+#download borg_backup
+wget $BORG_VERSION ./iso/netping/borg
 
 #clone repo with NpServerSettings
 git clone git@github.com:Netping/DKST90_NpServerSettings.git ./iso/netping/zabbix/NpServerSettings
@@ -44,10 +55,13 @@ cp /var/cache/apt/archives/*.deb ./iso/netping/deb/pgsql
 VERSION=$MAJOR_VERSION.$MINOR_VERSION.$PATH_VERSION.$BUILD_VERSION$(date '+%Y-%m-%dT%H:%M:%S')
 echo $VERSION > ./iso/netping/np_version
 
-#mv ./iso/ubuntu ./
-#cd ./iso/; find '!' -name "md5sum.txt" '!' -path "./isolinux/*" -follow -type f -exec "$(which md5sum)" {} ; > ../md5sum.txt
-#cd .. ; mv ./md5sum.txt ./iso/ ; mv ./ubuntu ./iso/
+cp -r ./sql ./iso/netping/sql
+
+
+mv ./iso/ubuntu ./
+cd ./iso/; find '!' -name "md5sum.txt" '!' -path "./isolinux/*" -follow -type f -exec "$(which md5sum)" {} ; > ../md5sum.txt
+cd .. ; mv ./md5sum.txt ./iso/ ; mv ./ubuntu ./iso/
 
 #combine to bootable iso 
-#xorriso -as mkisofs -r   -V Ubuntu\ NetpingZabbix   -o DKSL90.$VERSION.iso   -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot   -boot-load-size 4 -boot-info-table   -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot   -isohybrid-gpt-basdat -isohybrid-apm-hfsplus   -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin    iso/boot iso
+xorriso -as mkisofs -r   -V Ubuntu\ NetpingZabbix   -o DKSL90.$VERSION.iso   -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot   -boot-load-size 4 -boot-info-table   -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot   -isohybrid-gpt-basdat -isohybrid-apm-hfsplus   -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin    iso/boot iso
 
