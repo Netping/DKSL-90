eafca70e2e6b4c64c0fe60b8c5559e3ef81d9b18
Vadim <vv.lisyak@gmail.com>
2021-08-13 21:20:53 +0300

    DKSL90

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-#v0.2 4d4441@gmail.com
+#vv.lisyak@gmail.com
 
-if [ -f .ver ]
+if [ -f ver ]
 then
-  export $(cat .iso | sed 's/#.*//g' | xargs)
+  export $(cat ver | sed 's/#.*//g' | xargs)
 else 
-  echo ".ver Environment fine not exist"
+  echo "ver Environment fine not exist"
   exit 0
 fi
 apt update
@@ -26,49 +26,20 @@ fi
 rsync -av --progress /mnt/ ./iso/
 
 #clone repo with configs and sync with iso
-git clone git@github.com:Netping/DKSF_90.git ./isogit
+git clone -b '$MAJOR_VERSION.$MINOR_VERSION.$PATH_VERSION' git@github.com:Netping/DKSF_90.git ./isogit
 
 rsync -vr ./isogit/iso/ ./iso/
 rm -rf ./isogit 
 
-#download borg_backup
-if [ -f $(basename -- $BORG_VERSION) ]
-then
-  cp ./$(basename -- $BORG_VERSION) ./iso/netping/borg
-else
-  wget $BORG_VERSION
-  cp ./$(basename -- $BORG_VERSION) ./iso/netping/borg
-fi
+#Borg
+bash /tmp/iso/netping/backup/borg.sh
 
 #clone repo with NpServerSettings
-git clone git@github.com:Netping/DKST90_NpServerSettings.git ./iso/netping/zabbix/NpServerSettings
-
-#download and install zabbix repo 
-if [ -f $(basename -- $ZABBIX_VERSION) ]
-then
-  apt install ./$(basename -- $ZABBIX_VERSION)
-else
-  wget $ZABBIX_VERSION
-  apt install ./$(basename -- $ZABBIX_VERSION)
-fi
-
-#zabbix
-apt clean
-apt install --download-only -y zabbix-server-pgsql zabbix-frontend-php php7.4-pgsql zabbix-nginx-conf zabbix-agent
-cp /var/cache/apt/archives/*.deb ./iso/netping/deb/zabbix/
-cp ./zabbix-release_*_all.deb ./iso/netping/deb/zabbix/
-
-#postgresql 
-apt clean
-apt install --download-only -y postgresql
-cp /var/cache/apt/archives/*.deb ./iso/netping/deb/pgsql
+git clone -b '$MAJOR_VERSION.$MINOR_VERSION.$PATH_VERSION' git@github.com:Netping/DKST90_NpServerSettings.git /tmp/
+bash /tmp/DKST90_NpServerSettings/config.sh
 
 VERSION=$MAJOR_VERSION.$MINOR_VERSION.$PATH_VERSION.$BUILD_VERSION$(date '+%Y-%m-%dT%H:%M:%S')
 echo $VERSION > ./iso/netping/np_version
-
-#sql pgdb files --should be in git  
-#cp -r ./sql ./iso/netping/
-
 
 mv ./iso/ubuntu ./
 cd ./iso/; find '!' -name "md5sum.txt" '!' -path "./isolinux/*" -follow -type f -exec "$(which md5sum)" {} ; > ../md5sum.txt
@@ -76,4 +47,3 @@ cd .. ; mv ./md5sum.txt ./iso/ ; mv ./ubuntu ./iso/
 
 #combine to bootable iso 
 xorriso -as mkisofs -r   -V Ubuntu\ NetpingZabbix   -o DKSL90.$VERSION.iso   -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot   -boot-load-size 4 -boot-info-table   -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot   -isohybrid-gpt-basdat -isohybrid-apm-hfsplus   -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin    iso/boot iso
-
